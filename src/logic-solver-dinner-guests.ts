ef59785b3d9cab6969726654dfa1736b83534562
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2025-01-24 19:40:40 +0100

    remove extra commas

--- src/logic-solver-dinner-guests.ts
+++ src/logic-solver-dinner-guests.ts
@@ -9,20 +9,20 @@ const solution1 = solver.solve();
 console.log('\n');
 console.log(
   'Solution 1  ==> ',
-  solution1 ? solution1.getTrueVars() : solution1,
+  solution1 ? solution1.getTrueVars() : solution1
 );
 
 const solution2 = solver.solveAssuming('Alice');
 console.log(
   'Solution 2  ==> ',
-  solution2 ? solution2.getTrueVars() : solution2,
+  solution2 ? solution2.getTrueVars() : solution2
 );
 console.log(solution2.getMap());
 
 const solution3 = solver.solveAssuming(Logic.and('Alice', '-Charlie'));
 console.log(
   'Solution 3  ==> ',
-  solution3 ? solution3.getTrueVars() : solution3,
+  solution3 ? solution3.getTrueVars() : solution3
 );
 
 // List all possible solutions
